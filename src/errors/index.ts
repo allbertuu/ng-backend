73a471256c2e0d75fb337db06c6de6fbaab0dd37
Alberto <albertovinicius3@gmail.com>
2022-11-17 21:58:46 -0300

    feat(errors): create handleErrors function

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,18 +1,41 @@
+import { NextFunction, Request, Response } from 'express';
 // user já existe
 export const userAlreadyExists = {
-    message: "User already exists",
+    message: 'User already exists',
     statusCode: 409,
-    error: "Conflict",
+    error: 'Conflict',
 };
 // username ou password é inválido
 export const usernameOrPasswordInvalid = {
-    message: "Username or password is invalid",
+    message: 'Username or password is invalid',
     statusCode: 400,
-    error: "Bad Request",
+    error: 'Bad Request',
 };
 // username não está cadastrado
 export const userIsNotRegistered = {
-    message: "User is not registered",
+    message: 'User is not registered',
     statusCode: 404,
-    error: "Not Found",
+    error: 'Not Found',
 };
+
+export function handleErrors(
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) {
+    if (err.message === userAlreadyExists.message)
+        return res.status(userAlreadyExists.statusCode).json(userAlreadyExists);
+    if (err.message === usernameOrPasswordInvalid.message)
+        return res
+            .status(usernameOrPasswordInvalid.statusCode)
+            .json(usernameOrPasswordInvalid);
+    if (err.message === userIsNotRegistered.message)
+        return res
+            .status(userIsNotRegistered.statusCode)
+            .json(userIsNotRegistered);
+
+    return res
+        .status(500)
+        .json({ statusCode: 500, error: 'Internal server error' });
+}
